0537a0b5abf31149158dffe163365c3dfccf1aa8
ksushkin422 <47695589+ksushkin422@users.noreply.github.com>
2025-04-30 23:25:52 +0700

    Update detail_controller.dart

--- lib/modules/catalog/controllers/detail_controller.dart
+++ lib/modules/catalog/controllers/detail_controller.dart
@@ -1,9 +1,8 @@
-import 'dart:convert';
-
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:get_storage/get_storage.dart';
+import 'package:test_task/db/response/products_response.dart';
 
 import '../../../routes/app_pages.dart';
 
@@ -11,15 +10,15 @@ import '../../../routes/app_pages.dart';
 class DetailController extends GetxController {
 
   final count = 0.obs;
-  var product_item = GetStorage().read('product_item');
+  ProductsResponse product_item = GetStorage().read('product_item');
   var selected_image = ''.obs;
-  var product = {}.obs;
+  var product = Rxn<ProductsResponse>();
   var cart = [].obs;
 
   @override
   void onInit() {
     super.onInit();
-    selected_image.value = product_item['image'][0];
+    selected_image.value = product_item.image?.first;
     product.value = product_item;
     setPackaging();
     inititalSetPackaging();
@@ -36,36 +35,34 @@ class DetailController extends GetxController {
   }
 
   void setPackaging() {
-    product.value['packaging'].forEach((pack){
-      pack['selected'] = 0;
+    product.value?.packaging?.forEach((pack){
+      pack.selected = 0;
     });
   }
 
   void inititalSetPackaging() {
-    product.value['packaging'][0]['selected']=1;
+    product.value?.packaging?.first.selected=1;
   }
 
-  void selectPackaging(BuildContext context, item) {
+  void selectPackaging(BuildContext context, Packaging item) {
     setPackaging();
-    product.value['packaging'].where((pack)=>pack['id']==item['id']).toList()[0]['selected']=1;
+    product.value?.packaging?.where((pack)=>pack.id==item.id).toList()[0].selected=1;
     product.refresh();
   }
 
-  void addToCartFirst(BuildContext context, packaging_item){
-    print(packaging_item);
+  void addToCartFirst(BuildContext context, Packaging packaging_item){
     cart.add(
       {
-        "product_id": product_item['id'],
-        "packaging_id": packaging_item['id'],
-        "packaging_size": packaging_item['size'],
-        "packaging_price": packaging_item['price'],
+        "product_id": product_item.id,
+        "packaging_id": packaging_item.id,
+        "packaging_size": packaging_item.size,
+        "packaging_price": packaging_item.price,
         "count": 1,
-        "name": product.value['name'],
-        "image": product.value['image'][0],
-        "category": GetStorage().read('categories').where((categ) => categ['id'] == product_item['category_id']).toList()[0],
+        "name": product.value?.name,
+        "image": product.value?.image?[0],
+        "category": GetStorage().read('categories').where((categ) => categ['id'] == product_item.category_id).toList()[0],
       }
     );
-    print(cart);
     GetStorage().writeInMemory('cart', cart);
   }
 
@@ -74,8 +71,8 @@ class DetailController extends GetxController {
   }
 
   void removeItem(BuildContext context, item){
-    var selected = product.value['packaging'].where((item) =>
-    item['selected'] == 1).toList()[0]['id'];
+    var selected = product.value?.packaging?.where((item) =>
+    item.selected == 1).toList()[0].id;
     var item_in_cart = cart.where((prod_item)=>prod_item['product_id']==item['id']&&prod_item['packaging_id']==selected).toList()[0];
     if (item_in_cart['count']>1){
       cart.where((prod_item)=>prod_item['product_id']==item['id']&&prod_item['packaging_id']==selected).toList()[0]['count']--;
@@ -87,10 +84,10 @@ class DetailController extends GetxController {
     cart.refresh();
   }
 
-  void addItem(BuildContext context, item){
-    var selected = product.value['packaging'].where((item) =>
-    item['selected'] == 1).toList()[0]['id'];
-    cart.where((prod_item)=>prod_item['product_id']==item['id']&&prod_item['packaging_id']==selected).toList()[0]['count']++;
+  void addItem(BuildContext context, ProductsResponse? item_product){
+    var selected = product.value?.packaging?.where((Packaging item) =>
+    item.selected == 1).toList()[0].id;
+    cart.where((prod_item)=>prod_item['product_id']==item_product?.id&&prod_item['packaging_id']==selected).toList()[0]['count']++;
     GetStorage().writeInMemory('cart', cart);
     product.refresh();
     cart.refresh();
@@ -103,7 +100,6 @@ class DetailController extends GetxController {
 
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
   }
 
